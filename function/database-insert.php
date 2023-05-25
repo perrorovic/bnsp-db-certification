2618dc57676f9b443a0ecab6898fef316a7362fc
perrorovic <perrorovic@gmail.com>
2023-05-25 09:32:41 +0700

    icon directory fix

--- function/database-insert.php
+++ function/database-insert.php
@@ -36,7 +36,7 @@ if (isset($_POST["input"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" type="image/x-icon" href="bnsp-icon.png">
+    <link rel="icon" type="image/x-icon" href="../assets/bnsp-icon.png">
     <title>TUK-003.001100 - FR.IA.02.</title>
 </head>
 <style>
